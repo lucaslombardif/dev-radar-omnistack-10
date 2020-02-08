36cb9946e02fb667d10296c4d296b6ee3a6f8304
lucaslombardif <lucaas-floriano@hotmail.com>
2020-02-07 23:22:42 -0300

    Estilização concluída

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,49 +1,145 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import { MaterialIcons } from '@expo/vector-icons';
 
-function Main() {
-	const [currentRegion, setCurrentRegion] = useState(null);
+function Main({ navigation }) {
+  const [currentRegion, setCurrentRegion] = useState(null);
 
+  useEffect(() => {
 
-	useEffect(() => {
+    async function loadInitialPosition() {
+      const { granted } = await requestPermissionsAsync();
 
-		async function loadInitialPosition() {
-			const { granted } = await requestPermissionsAsync();
+      if (granted) {
+        const { coords } = await getCurrentPositionAsync({
+          enableHighAccuracy: true,
+        });
 
-			if (granted) {
-				const { coords } = await getCurrentPositionAsync({
-					enableHighAccuracy: true,
-				});
+        const { latitude, longitude } = coords;
 
-				const { latitude, longitude } = coords;
+        setCurrentRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.04,
+        })
+      }
+    }
 
-				setCurrentRegion({
-					latitude,
-					longitude,
-					latitudeDelta: 0.04,
-					longitudeDelta: 0.04,
-				})
-			}
-		}
+    loadInitialPosition();
 
-		loadInitialPosition();
+  }, []);
 
-	}, []);
+  if (!currentRegion) {
+    return null;
+  }
 
-	if(!currentRegion) { 
-		return null;
+  return (
+    <>
+      <MapView initialRegion={currentRegion} style={styles.map} >
+        <Marker coordinate={{ latitude: -27.19464, longitude: -49.6136825 }}>
+          <Image
+            style={styles.avatar}
+            source={{ uri: "https://avatars2.githubusercontent.com/u/23706340?s=460&v=4" }}
+          />
+          <Callout onPress={() => {
+            navigation.navigate('Profile', { github_username: 'lucaslombardif' });
+          }}>
+            <View style={styles.callout}>
+              <Text style={styles.devName}>Lucas Lombardi Floriano</Text>
+              <Text style={styles.devBio}>“O espírito humano precisa prevalecer sobre a tecnologia”. - Albert Einstein</Text>
+              <Text style={styles.devTechs}>ReactJS, Node.js, React Native</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+      <View style={styles.searchForm}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Buscar devs por techs..."
+          placeholderTextColor="#999"
+          autoCapitalize="words"
+          autoCorrect={false}
+        />
 
-	}
-
-	return <MapView initialRegion={currentRegion} style={styles.map} />
+        <TouchableOpacity onPress={() => { }} styles={styles.loadButton}>
+          <MaterialIcons name="my-location" size={20} color="#FFF" />
+        </TouchableOpacity>
+      </View>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-	map: {
-		flex: 1
-	},
+  map: {
+    flex: 1
+  },
+
+  avatar: {
+    width: 54,
+    height: 54,
+    borderRadius: 4,
+    borderWidth: 4,
+    borderColor: '#FFF'
+  },
+
+  callout: {
+    width: 260,
+  },
+
+  devName: {
+    fontWeight: 'bold',
+    fontSize: 16,
+
+  },
+
+  devBio: {
+    color: '#666',
+    marginTop: 5,
+  },
+
+  devTechs: {
+    marginTop: 5,
+  },
+
+  searchForm: {
+    position: 'absolute',
+    bottom: 20,
+    left: 20,
+    right: 20,
+    zIndex: 5,
+    display: 'flex',
+    flexDirection: 'row',
+  },
+
+  searchInput: {
+    flex: 1,
+    height: 50,
+    backgroundColor: '#FFF',
+    color: '#333',
+    borderRadius: 25,
+    paddingHorizontal: 20,
+    fontSize: 16,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 4,
+      height: 4,
+    },
+    elevation: 2,
+  },
+
+  loadButton: {
+    width: 50,
+    height: 50,
+    backgroundColor: '#8E4Dff',
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 15,
+  }
 })
 
 export default Main;
--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -5,27 +5,28 @@ import Main from './pages/Main';
 import Profile from './pages/Profile';
 
 const Routes = createAppContainer(
-    createStackNavigator({
-        Main: {
-            screen: Main,
-            navigationOptions: {
-                title: 'DevRadar'
-            },
-        },
-        Profile: {
-            screen: Profile,
-            navigationOptions: {
-                title: 'Perfil no Github'
-            }
-        },
-    }, {
-        defaultNavigationOptions: {
-            headerTintColor: '#FFF',
-            headerStyle: {
-                backgroundColor: '#7d40e7'
-            },
-        },
-    })
+  createStackNavigator({
+    Main: {
+      screen: Main,
+      navigationOptions: {
+        title: 'DevRadar'
+      },
+    },
+    Profile: {
+      screen: Profile,
+      navigationOptions: {
+        title: 'Perfil no Github'
+      }
+    },
+  }, {
+    defaultNavigationOptions: {
+      headerTintColor: '#FFF',
+      headerBackTitleVisible: false,
+      headerStyle: {
+        backgroundColor: '#7d40e7'
+      },
+    },
+  })
 );
 
 export default Routes;
